49f7b58f15b27cc5849603fb5c7df7aae150366f
melisa <irem.akel@ug.bilkent.edu.tr>
2023-07-18 15:29:57 +0300

    .

---
+++ spring-5-steps/src/test/java/com/spring/basics/spring5steps/Spring5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basics.spring5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
